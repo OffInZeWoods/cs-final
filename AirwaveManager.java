ce1ff6c9407720a6d07b3656038e996b6ccba921
Ethan Coover <ethancoover275@gmail.com>
2019-04-11 14:13:25 -0400

    inital commit

---
+++ AirwaveManager.java
@@ -0,0 +1,38 @@
+
+
+public class AirwaveManager {
+	private double frequency = 102.0;
+	private String currentlyPlaying = "";
+
+	public AirwaveManager() {
+
+	}
+
+	public AirwaveManager(double freq) {
+		frequency = freq;
+	}
+
+	public AirwaveManager(double freq, String startPlay) {
+		frequency = freq;
+		sendToAirwave(startPlay);
+	}
+
+	public void sendToAirwave(String songName) {
+		try {
+			//sudo ./fm_transmitter [-f frequency] [-r] filename
+			String toSend = "sudo ./fm_transmitter -f " + frequency + " -r " + songName;
+			Runtime.getRuntime().exec(toSend);
+			setCurrentlyPlaying(songName);
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+	}
+
+	public void setCurrentlyPlaying(String setAs) {
+		currentlyPlaying = setAs;
+	}
+
+	public String getCurrentlyPlaying() {
+		return currentlyPlaying;
+	}
+}
