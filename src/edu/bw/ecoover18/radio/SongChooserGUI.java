fc00ed140e3493bbf50c714658a754ee6126adc6
Ethan Coover <ethancoover275@gmail.com>
2019-04-18 14:26:43 -0400

    added SongChooserGUI

--- src/edu/bw/ecoover18/radio/SongChooserGUI.java
+++ src/edu/bw/ecoover18/radio/SongChooserGUI.java
@@ -1,4 +1,6 @@
-import javax.swing.JOptionPane;
+import java.io.File;
+import javax.swing.FileNameExtensionFilter;
+import javax.swing.JFileChooser;
 
 public class SongChooserGUI {
 	public SongChooserGUI() {
@@ -10,6 +12,13 @@ public class SongChooserGUI {
 	}
 
 	public void addSong() {
-
+		JFIleChooser fileChoose = new JFileChooser();
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("WAV only", "wav");
+		fileChoose.setFileFilter(filter);
+		int returnVal = chooser.showOpenDialog(null);
+		if(returnVal == JFileChooser.APPROVE_OPTION) {
+			File selected = fileChoose.getSelectedFile();
+			java.nio.file.Files.readAllBytes(Paths.get(selected.getAbsolutePath()));
+		}
 	}
 }
